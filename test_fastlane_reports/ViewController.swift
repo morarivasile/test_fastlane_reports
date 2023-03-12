1d4ffe2f900017c734bc825756cc5dc337721959
vasilemorari <32879809+morarivasile@users.noreply.github.com>
2023-03-12 22:46:14 +0200

    Created empty project

---
+++ test_fastlane_reports/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_fastlane_reports
+//
+//  Created by Vasile Morari on 12.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
