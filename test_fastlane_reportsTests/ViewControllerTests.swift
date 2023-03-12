1f816255ff73558fa42ecf8c00723d574c401681
vasilemorari <32879809+morarivasile@users.noreply.github.com>
2023-03-12 23:03:05 +0200

    Added snapshot test

---
+++ test_fastlane_reportsTests/ViewControllerTests.swift
@@ -0,0 +1,32 @@
+//
+//  ViewControllerTests.swift
+//  test_fastlane_reportsTests
+//
+//  Created by Vasile Morari on 12.03.2023.
+//
+
+import UIKit
+import XCTest
+import SnapshotTesting
+
+@testable import test_fastlane_reports
+
+final class ViewControllerTests: XCTestCase {
+	
+	private var sut: ViewController!
+	
+	override func setUp() {
+		super.setUp()
+		
+		sut = UIStoryboard(name: "Main", bundle: .main).instantiateInitialViewController()
+	}
+	
+	func testSnapshot() {
+		// when
+		sut.loadViewIfNeeded()
+		sut.setText("TESTING")
+		
+		// then
+		assertSnapshot(matching: sut, as: .image)
+	}
+}
